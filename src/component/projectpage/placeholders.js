5a4023439b723295f783725428542ec9106f7da9
Ren <raulnino24@gmail.com>
2020-06-08 16:27:54 -0400

    links

---
+++ src/component/projectpage/placeholders.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+
+function Placeholder (){
+    return (
+        <div classNameName="col-lg-12">
+        <div className="cards">
+        <div className="card" >
+        <img src= "../images/Dayplanner.png" className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title">Project one</h5>
+          <p className="card-text">.</p>
+          <a href="https://nino-on.github.io/Calender/" className="btn btn-primary">Calender</a>
+        </div>
+      </div>
+        <br></br>
+      <div className="card" >
+        <img src="..." className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title">Project one</h5>
+          <p className="card-text">.</p>
+          <a href="https://ezequiell323.github.io/groupproject01/" className="btn btn-primary">EventPlanner</a>
+        </div>
+        <br></br>
+        <div className="card" >
+        <img src="..." className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title">Project one</h5>
+          <p className="card-text">.</p>
+          <a href="https://nino-on.github.io/Project2/" className="btn btn-primary">Project2</a>
+        </div>
+
+        
+      
+      </div>
+      </div>
+      </div>
+      </div>
+
+    );
+}
+export default Placeholder;
--- src/component/projectpage/projectpage.js
+++ src/component/projectpage/projectpage.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Navbar from '../Navbar.js';
-
+import Placeholder from './placeholders'
 
 function Projects (){
     return (
      
         <div className="container">
         <Navbar/>
-      
+        <br></br>
+        <Placeholder/>    
       
         </div>
 
